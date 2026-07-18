566e38bfad995a70003f8d0e7261914ee02717ec
Roheithr07 <roheithr07@gmail.com>
2026-07-19 01:57:17 +0530

    Add program 002 - Variables

---
+++ programs/002_variables.py
@@ -0,0 +1,20 @@
+"""
+Program : 002
+Title    : Variables
+Author   : Roheith R
+Language : Python
+Description : Demonstrates variable declaration and different data types.
+"""
+
+name = "Roheith R"
+age = 19
+is_student = True
+
+print("Name :", name)
+print("Age :", age)
+print("Student :", is_student)
+
+print("\nData Types")
+print(type(name))
+print(type(age))
+print(type(is_student))
