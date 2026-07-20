cfa00bc8b911649ff632a47edb1d599394155bb6
Roheithr07 <roheithr07@gmail.com>
2026-07-20 22:26:17 +0530

    Add program 003 - User Input

---
+++ programs/003_user_input.py
@@ -0,0 +1,15 @@
+"""
+Program : 003
+Title    : User Input
+Author   : Roheith R
+Language : Python
+Description : Demonstrates how to accept input from the user and display the entered values.
+"""
+name=input("enter your name:")
+age=input("enter your age:")
+city=input("enter your city:")
+print("\n------enter your details------------")
+print("name:",name)
+print("age:",age)
+print("city:",city)
+print("-----------------------------------------")
